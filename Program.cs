cfb5451c24458d2345f41443234e802d1d5657b7
ghost <ghost@H2O_AQUA>
2024-01-21 23:47:02 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿/* this is a single-line comment
+ * this is a multi-line comment
+ * 
+ * Auther:   Christopher Lopez
+ * Course:   COMP-003A-L01
+ * Purpose:  Code for Assignment 1
+ */
+using System;
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About ME");
+            Console.WriteLine("1. I'm a Engineering major");
+            Console.WriteLine("2. I'm in the STEM field.");
+            Console.WriteLine("3. I am a okay problem solver.");
+            Console.WriteLine("4. I have a passion for technology.");
+            Console.WriteLine("5. I am a creative thinker sometimes.");
+            Console.WriteLine("6. I would like to explore different programming languages.");
+            Console.WriteLine("7. I am a team player.");
+            Console.WriteLine("8. I am self-motivated.");
+            Console.WriteLine("9. I pay attention to detail when I'm not rushing.");
+            Console.WriteLine("10. I am constantly seeking personal growth.");
+            Console.WriteLine("11. I enjoy collaborating with others.");
+            Console.WriteLine("12. I am adaptable to change.");
+            Console.WriteLine("13. I have good communication skills.");
+            Console.WriteLine("14. I strive for excellence.");
+            Console.WriteLine("15. I am dedicated to my work.");
+            Console.WriteLine("16. I have a strong work ethic.");
+            Console.WriteLine("17. I am open to feedback.");
+            Console.WriteLine("18. I believe in continuous learning.");
+            Console.WriteLine("19. I am curious.");
+            Console.WriteLine("20. I enjoy problem-solving challenges.");
+            Console.WriteLine("21. I am a good listener.");
+            Console.WriteLine("22. I am organized and detail-oriented.");
+            Console.WriteLine("23. I want to be able to write clean and efficient code.");
+            Console.WriteLine("24. I like gaming.");
+            Console.WriteLine("25. I like First person shooters.");
+            Console.WriteLine("26. I play Call Of duty games.");
+            Console.WriteLine("27. I play Fortnite.");
+            Console.WriteLine("28. I play Rust on PC.");
+            Console.WriteLine("29. I play Arma 3 on pc.");
+            Console.WriteLine("30. I also have a Switch Console.");
+            Console.WriteLine("31. I play Pokemon on it.");
+            Console.WriteLine("32. I play Luigi's Mansion.");
+            Console.WriteLine("33. I play don't starve.");
+            Console.WriteLine("34. I've also played Animal Crossing.");
+            Console.WriteLine("35. I like to go to the gym.");
+            Console.WriteLine("36. I'm working on losing weight & gaining musle mass.");
+            Console.WriteLine("37. I enjoy working on both individual and team projects.");
+            Console.WriteLine("38. I am a quick learner if I put in the work.");
+            Console.WriteLine("39. I am a pro-gun person.");
+            Console.WriteLine("40. I enjoy going to the range to shoot.");
+            Console.WriteLine("41. I have a Glock 17.");
+            Console.WriteLine("42. I also have a Glendfield 30-30A lever action rifle.");
+            Console.WriteLine("43. I am working on getting my CCW Permit.");
+            Console.WriteLine("44. I did graduate highschool.");
+            Console.WriteLine("45. I played basketball in highschool.");
+            Console.WriteLine("46. I've worked at Mcdonald's.");
+            Console.WriteLine("47. I did become a manager in 8 months.");
+            Console.WriteLine("48. I've worked at Subway.");
+            Console.WriteLine("49. My Subway rank was Senior Sandwich Artist");
+            Console.WriteLine("50. Currently working at AmPm gas station.");
+            Console.WriteLine("51. I like to Bass Pro Shops.");
+            Console.WriteLine("52. I like Nissan GTR's.");
+            Console.WriteLine("53. The next car I want is either a R33 or a R35.");
+            Console.WriteLine("54. I have a 2017 Nissan Altima SV.");
+            Console.WriteLine("55. I also have a 2006 Chevy Silverado 1500.");
+            Console.WriteLine("56. I'm taking 16 credits this semester.");
+            Console.WriteLine("57. I would like to travle more this year.");
+            Console.WriteLine("58. My top 5 places I want to visit are.");
+            Console.WriteLine("59. Japan for the culture.");
+            Console.WriteLine("60. Iceland durning the summer.");
+            Console.WriteLine("61. Toronto to see the Niagra Falls.");
+            Console.WriteLine("62. Alaska to experience the Northern Lights .");
+            Console.WriteLine("63. Last i would like to visit Hawaii or guam.");
+            Console.WriteLine("64. I like chicken.");
+            Console.WriteLine("65. I like sushi.");
+            Console.WriteLine("66. I like salmon.");
+            Console.WriteLine("67. I like squid.");
+            Console.WriteLine("68. I like to cook.");
+            Console.WriteLine("69. I'm a car person.");
+            Console.WriteLine("70. I would like to go skydiving soon.");
+            Console.WriteLine("71. I like anime.");
+            Console.WriteLine("72. I like the Dragon ball series.");
+            Console.WriteLine("73. I like the Demon slayer series.");
+            Console.WriteLine("74. I like the Jujutsu kaisen series.");
+            Console.WriteLine("75. I like the Baki series.");
+            Console.WriteLine("76. I like the Naruto series.");
+            Console.WriteLine("77. I like the Initial D series.");
+            Console.WriteLine("78. I'm a big Star Wars fan.");
+            Console.WriteLine("79. I have a crazy Funko pop! collection.");
+            Console.WriteLine("80. I like the Marvel movies and shows.");
+            Console.WriteLine("81. I Like The Office series.");
+            Console.WriteLine("82. I think Family Guy is hilarious.");
+            Console.WriteLine("83. I'm part of the Engineering the Future club in West Hills.");
+            Console.WriteLine("84. I'm in the Math, Engineering & Science Achievment program.");
+            Console.WriteLine("85. I'm first generation college sudent.");
+            Console.WriteLine("86. I enjoy driving in foggy and rainy days.");
+            Console.WriteLine("87. I am dedicated to continuous improvement.");
+            Console.WriteLine("88. I am always busy and seem to put too much on my plate a lot.");
+            Console.WriteLine("89. This is my second coding class.");
+            Console.WriteLine("90. The more I learn and experience different coding lauguages, the less I'm frighten and want ot learn more.");
+            Console.WriteLine("91. I Like out door adventures.");
+            Console.WriteLine("92. I would like to go hiking more.");
+            Console.WriteLine("93. I like tamales.");
+            Console.WriteLine("94. I like tacos.");
+            Console.WriteLine("95. I like burritos.");
+            Console.WriteLine("96. I perfer the winter.");
+            Console.WriteLine("97. I sleep with a blanket.");
+            Console.WriteLine("98. I help my dad sometimes with his side job.");
+            Console.WriteLine("99. I would like to be in the Air Force.");
+            Console.WriteLine("100. I'm thinking of joining the Navy.");
+
+            Console.ReadLine();
+        }
+    }
+}
